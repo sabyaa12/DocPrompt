e7d7ba1729e11cc7e5bb958dc50c6aea8ec34e33
sabyasachi40 <b322040@iiit-bh.ac.in>
2025-08-25 00:19:44 +0530

    small fix of navbar name

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar.jsx';
 import Sidebar from './components/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Admin/Dashboard';
